aeb179aea1ceb2d02429abe10f2be4d86650505a
Diana Zazueta <42899991+ZazuetaDiana@users.noreply.github.com>
2020-11-26 00:01:20 -0800

    Update teclado.s

--- teclado.s
+++ teclado.s
@@ -3,8 +3,8 @@
 @ programa corto que obtiene la entrada del teclado y luego lo imprime de nuevo en la pantalla.
 
 
-.sección 	.bss
-.comm búfer, 48 	     @ reserva búfer de 48 bytes
+.section 	.bss
+.comm buffer, 48 	     @ reserva búfer de 48 bytes
 
 .section 	.data
 msg:
@@ -14,19 +14,19 @@ msg2:
 	.ascii 	"Hola"
 msg2Len = . - msg2
 
-.sección 	.texto
+.section 	.text
 .globl 	_start
-_comienzo:
+_start:
 
 mov r0, $ 1 		    @ mensaje de apertura del programa de impresión	
-ldr r1, = mensaje
+ldr r1, = msg
 ldr r2, = msgLen
 mov r7, $ 4
 svc $ 0
 
 mov r7, $ 3 		    @ read syscall
 mov r0, $ 1		
-ldr r1, = búfer
+ldr r1, = buffer
 mov r2, $ 0x30
 svc $ 0
 
@@ -37,11 +37,11 @@ mov r7, $ 4
 svc $ 0
 
 mov r0, $ 1 		    @ ahora imprime la entrada del usuario
-ldr r1, = búfer
+ldr r1, = buffer
 mov r2, $ 0x30
 mov r7, $ 4
 svc $ 0
 
 mov r7, $ 1 	            @ salir de syscall
 svc $ 0 		            @ wake kernel
-.fin
+.end
